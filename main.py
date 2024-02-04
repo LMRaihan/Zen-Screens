93b93ae0ed528d4ce7d768848f485f880df96353
LMRaihan <lmraihanhamid@gmail.com>
2024-02-05 04:46:23 +0600

    Comment updated

--- main.py
+++ main.py
@@ -1,25 +1,44 @@
-import pygame , time
-pygame.init()
-screenWidth, screenHeight = 1920, 1080
-logoSpeed = [1, 1]
-backgroundColor = 0, 0, 0
+import pygame
+import sys
 
-screen = pygame.display.set_mode((screenWidth,screenHeight))
+pygame.init()
+
+screenWidth, screenHeight = 1000, 800
+logoSpeed = [2, 2]
+backgroundColor = (0, 0, 0)
+
+#creating a Pygame screen with the specified dimensions
+screen = pygame.display.set_mode((screenWidth, screenHeight))
+
+pygame.display.set_caption("Zen Screens") #window-screen
 
 logo = pygame.image.load("x.jpg")
-logo = pygame.transform.scale(logo,(100,100))
-logoRect = logo.get_rect()
+logo = pygame.transform.scale(logo, (200, 200))
+logoRect = logo.get_rect() #rectangle object associated with "x"
 
+#Create a Pygame clock object to control the frame rate
+clock = pygame.time.Clock()
+
+#QUIT handling loop
 while True:
-  screen.fill(backgroundColor)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit() 
 
-  screen.blit(logo,logoRect)
-  logoRect = logoRect.move(logoSpeed)
+    screen.fill(backgroundColor) 
+    #draw the logo image onto the screen at its current position.
+    screen.blit(logo, logoRect) 
+    # Move the logo rectangle by the specified speed       
+    logoRect = logoRect.move(logoSpeed) 
 
-  if logoRect.left < 0 or logoRect.right > screenWidth:
-    logoSpeed[0] = -logoSpeed[0]
-  if logoRect.top < 0 or logoRect.bottom > screenHeight:
-    logoSpeed[1] = -logoSpeed[1]
+  #Bounce the logo off the screen edges
+    if logoRect.left < 0 or logoRect.right > screenWidth:
+        logoSpeed[0] = -logoSpeed[0]
+    if logoRect.top < 0 or logoRect.bottom > screenHeight:
+        logoSpeed[1] = -logoSpeed[1]
 
-  pygame.display.flip()
-  time.sleep(10/1000)
+  #the display to show the changes made during this iteration of the game loop.
+    pygame.display.flip()
+  # Adjust the frame rate as needed                              
+    clock.tick(60)   
